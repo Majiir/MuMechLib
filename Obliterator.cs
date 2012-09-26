d0591ba7681be5b8a8f7ba2dc5534fc7900c07cc
r4m0n <r4m0n@37bd7454-cb9a-4765-8a6b-8adff431c72b>
2012-09-26 04:55:20 +0000

    Added The Obliterator.
    git-svn-id: http://svn.mumech.com/KSP/trunk/MuMechLib@67 37bd7454-cb9a-4765-8a6b-8adff431c72b

---
+++ Obliterator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class MuMechObliteratorGhost : MonoBehaviour
+{
+    public string planetName;
+    public string parentName;
+    public List<string> targetNames;
+    public MuMechObliterator part;
+
+    void Start()
+    {
+        planetName = part.planetName;
+        parentName = part.parentName;
+        targetNames = part.targetNames.Split("/".ToCharArray()).ToList<string>();
+    }
+
+    void Update()
+    {
+        if (FlightGlobals.ready)
+        {
+            foreach (OrbitDriver o in Planetarium.Orbits)
+            {
+                if (o.name == planetName)
+                {
+                    GameObject planet = o.gameObject;
+                    Transform parentObject = o.transform.Find(parentName);
+
+                    if (parentObject != null)
+                    {
+                        for (int i = 0; i < parentObject.childCount; i++)
+                        {
+                            foreach (string n in targetNames)
+                            {
+                                if (parentObject.GetChild(i).name.StartsWith(n))
+                                {
+                                    GameObject target = parentObject.GetChild(i).gameObject;
+                                    print("Obliterator - Destroying " + target.name);
+                                    target.transform.parent = null;
+                                    GameObject.Destroy(target);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class MuMechObliterator : Part
+{
+    public string planetName = "";
+    public string parentName = "";
+    public string targetNames = "";
+
+    protected override void onPartLoad()
+    {
+        GameObject ghost = new GameObject("obliteratorGhost_" + name);
+        MuMechObliteratorGhost og = ghost.AddComponent<MuMechObliteratorGhost>();
+        og.part = this;
+        GameObject.DontDestroyOnLoad(ghost);
+        base.onPartLoad();
+    }
+
+    protected override void onPartStart()
+    {
+        if (GameObject.Find("interior_vehicleassembly") != null)
+        {
+            GameObject.Destroy(GameObject.Find("interior_vehicleassembly"));
+            Camera.mainCamera.GetComponent<VABCamera>().maxHeight *= 100;
+            Camera.mainCamera.GetComponent<VABCamera>().maxDistance *= 100;
+        }
+    }
+}
